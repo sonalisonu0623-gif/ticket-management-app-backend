5b220ecc4d83a9513bc3f669baa25688018798eb
Sonali <sonalisonu0623@gmail.com>
2026-05-28 15:49:37 +0530

    new code

--- backend/src/main/java/com/ticketsystem/entity/Ticket.java
+++ backend/src/main/java/com/ticketsystem/entity/Ticket.java
@@ -47,7 +47,7 @@ public class Ticket {
     private LocalDateTime responseDateTime;
 
     @Column(name = "resolution_time")
-    private String resolutionTime;
+    private LocalDateTime resolutionTime;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "current_status", nullable = false, length = 20)
